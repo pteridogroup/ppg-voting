33055b0280244ccdcc884815b12bb1d73c1dd765
joelnitta <joelnitta@gmail.com>
2024-08-02 19:36:34 +0900

    Process votes for Ballot 12

--- R/tally.R
+++ R/tally.R
@@ -7,9 +7,9 @@ source("R/functions.R")
 source("R/draft_ppg_results_email.R")
 
 # Set variables - change this each time ballot is tallied
-ballot_number <- "11"
-vote_period <- "June 2024"
-ballot_file <- "https://docs.google.com/spreadsheets/d/1W8m-CA1ePJT28lTOEUb8J-cZsKeToCv_LD8b_Xl9w7o/edit?usp=sharing" # nolint
+ballot_number <- "12"
+vote_period <- "July 2024"
+ballot_file <- "https://docs.google.com/spreadsheets/d/1GQ2oaAtrFQvSzU9s6RsUp73Iodgqks3yjOizdO5G4lw/edit?usp=sharing" # nolint
 
 # Check ballots
 ballot_checked <- check_ballot(
