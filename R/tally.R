e186df281191db587b62ba9abe05760c4313b508
joelnitta <joelnitta@gmail.com>
2026-01-03 11:43:15 +0900

    Tally ballot 20

--- R/tally.R
+++ R/tally.R
@@ -7,9 +7,9 @@ source("R/functions.R")
 source("R/draft_ppg_results_email.R")
 
 # Set variables - change this each time ballot is tallied
-ballot_number <- "19"
-vote_period <- "September 2025"
-ballot_file <- "https://docs.google.com/spreadsheets/d/1d9qFWehZI9nNaFtAM_gFQSO8Gie2N8aXQRFE_HG9UVA/edit?usp=sharing" # nolint
+ballot_number <- "20"
+vote_period <- "December 2025"
+ballot_file <- "https://docs.google.com/spreadsheets/d/1JuRQJLgShOtruY1MzxwgDHTPi-bxwlq1jJoVeD5s9nQ/edit?usp=sharing" # nolint
 
 # Check ballots
 ballot_checked <- check_ballot(
